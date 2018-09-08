9e5855776112f89d181e4ba51c20b5c17a3fb163
KokinSSJ <31051991@interia.pl>
2018-09-08 22:06:46 +0200

    karma many files works but only from command calling nodejs, but not gradle (not yet)

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,29 @@
-module.exports = function(config) { config.set({
+module.exports = function(config) { 
+function mergeFilesWithArgv(staticFiles) {
+
+    var source = staticFiles, argv = process.argv;
+
+    argv.forEach(function (arg) {
+        var index = arg.indexOf('--check=');
+        if (index !== -1) {
+            source.push(arg.substring(8));
+        }
+    });
+
+    return source;
+
+}
+
+
+config.set({
     "basePath": "",
     "colors": true,
     "logLevel": "INFO",
-    "files": [
+    "files": mergeFilesWithArgv([
         "jquery.js",
         "webapp/resources/**/*.js",
         "js-spec/**/*test.js"
-    ],
+    ]),
     "browsers": [
         "PhantomJS"
     ],
