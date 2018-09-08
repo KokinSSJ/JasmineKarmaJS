2157685fa8e2506ae62bf76c78849a1cebb094ed
KokinSSJ <31051991@interia.pl>
2018-09-08 22:50:27 +0200

    karma supports start with files passed via command --file=<file dir and name>

--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,16 @@ function mergeFilesWithArgv(staticFiles) {
 
     argv.forEach(function (arg) {
         var index = arg.indexOf('--check=');
-        if (index !== -1) {
-            source.push(arg.substring(8));
-        }
-    });
-
+		if(index !== -1) {
+			var elements = arg.split('--check=');
+			elements.forEach(function(element) {
+			 if(element !== "") {
+				 source.push(element);
+			 }
+			});
+		}    
+    });  
+	console.log(source);
     return source;
 
 }
